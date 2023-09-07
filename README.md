9f5a7c70b457eba67241d7cc8325f8f71b51911f
Mikael Forsberg <mkforsb@gmail.com>
2023-09-07 19:55:08 +0200

    feat: add docker containerization

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Introduction
 -
 mfMathPaint is a modular multiuser web application paint program focused on mathematics. PHP/MySQL/JavaScript.
 
-![Screenshot](http://lur.h1x.com/bathweirdo/ga/v2/pic.png "Screenshot")
+![Screenshot](https://raw.githubusercontent.com/mkforsb/mfmathpaint/master/doc/screenshot.png)
 
 Installation
 -
@@ -12,6 +12,14 @@ Installation
     /mfmathpaint$ editor db_config.php
     /mfmathpaint$ cd ..
 	$ cp index.html-dist index.html
+
+Docker
+-
+The [docker](https://github.com/mkforsb/mfmathpaint/tree/master/docker) folder contains a `Dockerfile` to build
+an mfMathPaint image based on [php:8.1-apache-bullseye](https://hub.docker.com/layers/library/php/8.1-apache-bullseye/images/sha256-b1eae7da0f50e4e7e9137348a0f6e8d2229ee3722f8632edfa11dc4a9dbf58de?context=explore), along with a 
+`docker-compose.yml` file to build and run mfMathPaint together with a stock MariaDB container ([mariadb:10.5](https://hub.docker.com/layers/library/mariadb/10.5/images/sha256-aa1ccc18000c32d1f39ac0b055117b27bffd93e622ec961d682de40fe2a1a95f?context=explore))
+
+    $ docker compose -f docker/docker-compose.yml up
 
 Enabling LaTeX
 -
---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [ ! -f "/etc/mfmathpaint-docker-setup-completed" ]; then
+	OLDIFS="$IFS"
+	IFS=
+
+	read -r -d '' script <<-EOSCRIPT
+		<?php
+		\$dsn = 'mysql:dbname=mfmathpaint;host=${MARIADB_HOST}';
+		\$user = '${MARIADB_USER}';
+		\$password = '${MARIADB_PASSWORD}';
+		for (\$i = 0; \$i < 60; ++\$i) {
+			try {
+				\$dbh = new PDO(\$dsn, \$user, \$password);
+				echo 'Successfully connected to database' . PHP_EOL;
+				break;
+			} catch (Exception \$e) {
+				echo 'Failed connecting to database, retrying in 5 seconds ...' . PHP_EOL;
+				sleep(5);
+			}
+		}
+		\$lines = explode("\\n", file_get_contents('/var/www/html/mfmathpaint/setup.sql'));
+		foreach (\$lines as \$line) {
+			if (\$line != '') {
+				\$dbh->exec(\$line);
+			}
+		}
+	EOSCRIPT
+
+	echo "$script" > /tmp/setup.php
+	php /tmp/setup.php
+
+	read -r -d '' config <<-EOCONFIG
+		<?php
+		define('MFMATHPAINT_DB_HOST', '${MARIADB_HOST}');
+		define('MFMATHPAINT_DB_USER', '${MARIADB_USER}');
+		define('MFMATHPAINT_DB_PASS', '${MARIADB_PASSWORD}');
+		define('MFMATHPAINT_DB_DBNAME', 'mfmathpaint');
+	EOCONFIG
+
+	echo "$config" > /var/www/html/mfmathpaint/db_config.php
+
+	touch /etc/mfmathpaint-docker-setup-completed
+
+	IFS="$OLDIFS"
+fi
+
+exec /usr/local/bin/docker-php-entrypoint apache2-foreground "$@"
