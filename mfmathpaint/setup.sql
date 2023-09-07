0d0aab384b8d297cad29b5894ed5396ead4bb1bc
Mikael Forsberg <mkforsb@gmail.com>
2023-09-07 19:30:42 +0200

    fix: make setup.sql tolerant of repeated runs

--- mfmathpaint/setup.sql
+++ mfmathpaint/setup.sql
@@ -1,3 +1,4 @@
-create database mfmathpaint;
+create database if not exists mfmathpaint;
 use mfmathpaint;
+drop table if exists data;
 create table data (instr_id INT(5) PRIMARY KEY AUTO_INCREMENT, origin SMALLINT(5) NOT NULL, instr VARCHAR(5) NOT NULL, obj_id INT(5), data1 INT(5), data2 INT(5), data3 INT(5), data4 INT(5), text TEXT, ttl INT(11));
